08310d888c82cd74c2b9739eaca3108aac341b6a
krllkrbv <krllkrbv@mail.ru>
2025-08-02 11:55:40 +0300

    bin

---
+++ bin/gendiff.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format: "stylish", "plain" or "json"', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
+
+program.parse(process.argv);
