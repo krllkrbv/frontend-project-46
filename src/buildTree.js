5d7c85d3b917d560d35955841035ea0a686f1c1d
krllkrbv <krllkrbv@mail.ru>
2025-08-06 16:59:01 +0300

    src

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,23 +1,23 @@
-import _ from 'lodash'
+import _ from 'lodash';
 
 const buildTree = (obj1, obj2) => {
-  const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)))
+  const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
 
   return keys.map((key) => {
     if (!Object.hasOwn(obj1, key)) {
-      return { key, type: 'added', value: obj2[key] }
+      return { key, type: 'added', value: obj2[key] };
     }
     if (!Object.hasOwn(obj2, key)) {
-      return { key, type: 'deleted', value: obj1[key] }
+      return { key, type: 'deleted', value: obj1[key] };
     }
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-      return { key, type: 'nested', children: buildTree(obj1[key], obj2[key]) }
+      return { key, type: 'nested', children: buildTree(obj1[key], obj2[key]) };
     }
     if (!_.isEqual(obj1[key], obj2[key])) {
-      return { key, type: 'changed', oldValue: obj1[key], newValue: obj2[key] }
+      return { key, type: 'changed', oldValue: obj1[key], newValue: obj2[key] };
     }
-    return { key, type: 'unchanged', value: obj1[key] }
-  })
-}
+    return { key, type: 'unchanged', value: obj1[key] };
+  });
+};
 
-export default buildTree
+export default buildTree;
