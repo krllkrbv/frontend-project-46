aad8e3e7f4f82a4760e215f0e67a2764799ad261
krllkrbv <krllkrbv@mail.ru>
2025-08-06 12:17:33 +0300

    eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,6 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig({
-  files: ["**/*.{js,mjs,cjs}"],
-  plugins: {
-    js,
-    stylistic: require('eslint-plugin-stylistic'),
-  },
-  extends: ["js/recommended"],
-  languageOptions: {
-    globals: globals.browser,
-  },
-  rules: {
-    '@stylistic/semi': ['error', 'always'],
-  },
-});
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+]);
